58c0ef366af3d2289b1006ae5a7dc012f8d1b65a
Anthony-Flowers-Student <anfla37@student.oaisd.org>
2024-10-10 17:17:51 +0000

    Wrote the code specified in the google classroom instructions. Replaced names and age with my own.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Anthony Flowers.");
+
+        //Using Variables
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Anthony";
+        string lastname = "Flowers";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age +" years old. I just spent $" + money + " on an energy drink.");
+
+    }
+}
